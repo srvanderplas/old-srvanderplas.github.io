337842a817baa18b3cd166913ad8aec1b144a5e6
Susan VanderPlas (root) <srvanderplas@gmail.com>
2015-07-27 13:12:11 -0500

    Data and code to install essential packages

---
+++ Code/InstallPackages.R
@@ -0,0 +1,15 @@
+# Essential R packages
+
+# Read in package list
+pkgs <- read.csv("Data/Packages.csv", stringsAsFactors = F)
+
+# Separate out github packages
+gh <- subset(pkgs, Location=="github")
+cran <- subset(pkgs, Location=="CRAN")
+
+# Install cran packages
+install.packages(cran$Name, Ncpus = 4, dependencies = c('Suggests', 'Depends', 'Imports', 'Enhances'))
+
+# Install github packages
+library(devtools)
+install_github(sprintf("%s/%s", gh$Author, gh$Name), dependencies = c('Suggests', 'Depends'))
