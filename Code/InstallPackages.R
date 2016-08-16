9fb03e25218dde233b9c23b3beda0eaeaaf250a0
VanderPlas <s1vande@prd.nppdnet.com>
2016-08-16 15:08:12 -0500

    Updated code and packages

--- Code/InstallPackages.R
+++ Code/InstallPackages.R
@@ -1,16 +1,17 @@
 # Essential R packages
 
 # Read in package list
-pkgs <- read.csv("https://raw.githubusercontent.com/srvanderplas/srvanderplas.github.io/master/Data/Packages.csv", stringsAsFactors = F, comment.char = "#")
-# pkgs <- read.csv("Data/Packages.csv", stringsAsFactors = F)
+pkgs <- read.csv("https://raw.githubusercontent.com/srvanderplas/srvanderplas.github.io/master/Data/Packages.csv",
+                 stringsAsFactors = F, comment.char = "#")
+# pkgs <- read.csv("Data/Packages.csv", stringsAsFactors = F, comment.char = "#")
 
 # Separate out github packages
-gh <- subset(pkgs, Location=="github")
-cran <- subset(pkgs, Location=="CRAN")
+gh <- subset(pkgs, Location == "github")
+cran <- subset(pkgs, Location == "CRAN")
 
 # Install cran packages
 install.packages(cran$Name, Ncpus = 4, dependencies = c('Suggests', 'Depends', 'Imports', 'Enhances'))
 
 # Install github packages
 library(devtools)
-install_github(sprintf("%s/%s", gh$Author, gh$Name), dependencies = c('Suggests', 'Depends'))
+install_github(sprintf("%s/%s", gh$Author, gh$Name))
